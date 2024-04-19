7724b8fd4e605b5329a79ac75158caee981aa0d8
David <75451291+dulkoss@users.noreply.github.com>
2024-04-19 19:09:33 +0200

    wip

--- src/CachedComponent.php
+++ src/CachedComponent.php
@@ -12,10 +12,20 @@ abstract class CachedComponent extends Component
 {
     use CachesValue;
 
+    protected function registerParameters(array $args): void
+    {
+        $parameters = (new \ReflectionClass(static::class))->getConstructor()->getParameters();
+        $this->parameters = collect($args)->mapWithKeys(fn ($v, $k) => [$parameters[$k]->name => $v])->toArray();
+    }
+
     /** {@inheritdoc} */
     public function resolveView()
     {
-        if (null !== $cache = $this->get()) {
+        if ($this->isUpdating()) {
+            return $this->get($this->parameters) ?? parent::resolveView();
+        }
+
+        if (null !== $cache = $this->get($this->parameters, update: true)) {
             return new HtmlString((string) $cache);
         }
 
