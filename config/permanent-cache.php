228fda5ea095ae975fa5cbd7ae7f252cca814f0d
Mark van Eijk <mark@vaneijk.co>
2024-04-27 10:44:27 +0200

    wip

--- config/permanent-cache.php
+++ config/permanent-cache.php
@@ -2,8 +2,8 @@
 
 return [
     'components' => [
-        // Add markers around the rendered value of Cached Components.
-        // This helps to identify the cached value in the rendered HTML.
+        // Add markers around the rendered value of Cached Components,
+        // this helps to identify the cached value in the rendered HTML.
 
         // Which is useful for debugging and testing, but also for updating
         // the cached value inside another cache when using nested caches
