995144abebb5a514f4777149201703d36e5d33d5
Mark van Eijk <mark@vaneijk.co>
2024-05-09 14:02:29 +0200

    wip

--- src/Commands/PermanentCachesStatusCommand.php
+++ src/Commands/PermanentCachesStatusCommand.php
@@ -5,6 +5,7 @@ namespace Vormkracht10\PermanentCache\Commands;
 use Illuminate\Console\Command;
 use ReflectionClass;
 use Spatie\Emoji\Emoji;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Vormkracht10\PermanentCache\Facades\PermanentCache;
 
 class PermanentCachesStatusCommand extends Command
@@ -14,7 +15,7 @@ class PermanentCachesStatusCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'permanent-cache:status {--parameters}';
+    protected $signature = 'permanent-cache:status {--P|parameters}';
 
     /**
      * The console command description.
@@ -30,22 +31,29 @@ class PermanentCachesStatusCommand extends Command
     {
         $caches = PermanentCache::configuredCaches();
 
-        foreach ($caches as $value) {
+        foreach ($caches as $c) {
             $cache = $caches->current();
             $parameters = $caches->getInfo();
 
-            $cachesTable[] = [
-                $cache->isCached($parameters) ? Emoji::checkMarkButton() : '',
+            $row = [
+                $cache->isCached($parameters) ? Emoji::checkMarkButton() : Emoji::crossMarkButton(),
                 (new ReflectionClass($cache))->getName(),
-                strlen($cache->get($parameters)),
+                readable_size(strlen($cache->get($parameters))),
                 '',
-                $this->option('parameters') ? $this->parseParameters($parameters) : null,
             ];
+
+            if ($this->option('parameters')) {
+                $row[] = $this->parseParameters($parameters);
+            }
+
+            $cachesTable[] = $row;
+            $cachesTable[] = new TableSeparator();
         }
 
         $this->table(
-            $this->option('parameters') ? ['', 'Cache', 'Parameters', 'Size', 'Last Updated'] : ['', 'Cache', 'Size', 'Last Updated'],
+            [null, 'Cache', 'Size', 'Last Updated'] + ($this->option('parameters') ? ['Parameters'] : []),
             $cachesTable,
+            'box',
         );
     }
 
