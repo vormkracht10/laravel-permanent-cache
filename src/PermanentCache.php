3bd1d8000e870ad9dcf6968473bc764cd255add8
Mark van Eijk <mark@vaneijk.co>
2024-05-10 21:01:07 +0200

    revert event listen

--- src/PermanentCache.php
+++ src/PermanentCache.php
@@ -46,7 +46,7 @@ class PermanentCache
 
                 if ([] !== $events = $cacheInstance->getListenerEvents()) {
                     foreach($events as $event) {
-                        Event::listen($event, fn () => $cacheInstance->handle());
+                        Event::listen($event, $cacheInstance);
                     }
                 }
 
