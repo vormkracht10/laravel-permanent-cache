0c8fbbbd59852882aeda7fc55e222157f16f3b43
Mark van Eijk <mark@vaneijk.co>
2024-05-16 14:41:12 +0200

    remove methods not used

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,29 +8,10 @@ use Vormkracht10\PermanentCache\PermanentCacheServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Vormkracht10\\PermanentCache\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             PermanentCacheServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-permanent-cache_table.php.stub';
-        $migration->up();
-        */
     }
 }
